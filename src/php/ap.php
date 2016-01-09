6d5d1b5704cbd310825da5f59be85226fc836d9f
jaswsinc <jaswsinc@wsharks.com>
2016-01-09 11:35:42 -0900

    Adding xclip util, pbcopy, pbpaste, and fixing a bug in the PHP v5.4 installer.

--- src/php/ap.php
+++ src/php/ap.php
@@ -1,14 +1,11 @@
 <?php
-declare (strict_types = 1);
 namespace WebSharks\Ubuntu\Bootstrap;
-
 // This is loaded via INI `auto_prepend_file`.
+// No strict types. This must be compatible w/ PHP v5.4+.
 
-(function () {
-    foreach ($_SERVER as $_key => &$_value) {
-        if ($_value === ' ' && strpos($_key, 'CFG_') === 0) {
-            // See: `src/setups/env-vars` for reasoning.
-            $_value = ''; // Empty this string.
-        }
-    } // unset($_key, $_value);
-})();
+foreach ($_SERVER as $_key => &$_value) {
+    if ($_value === ' ' && strpos($_key, 'CFG_') === 0) {
+        // See: `src/setups/env-vars` for reasoning.
+        $_value = ''; // Empty this string.
+    }
+} unset($_key, $_value);
