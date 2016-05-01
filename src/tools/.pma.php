d4277e729e7d692a5c9299de22cb5dc8c6f69ca3
jaswsinc <jaswsinc@wsharks.com>
2016-04-30 19:42:11 -0800

    Bug fix. `/---tools/` fastCGI params. Corrects and issue with PMA.

--- src/tools/.pma.php
+++ src/tools/.pma.php
@@ -2,9 +2,9 @@
 declare (strict_types = 1);
 namespace WebSharks\Ubuntu\Bootstrap\Tools;
 
-ini_set('session.save_path', '/tmp');
-ini_set('session.gc_maxlifetime', '86400');
+$cfg['SessionSavePath']     = '/tmp';
 $cfg['LoginCookieValidity'] = 86400;
+ini_set('session.gc_maxlifetime', '86400');
 
 $cfg['Servers'][1]['host'] = $_SERVER['CFG_MYSQL_DB_HOST'];
 $cfg['Servers'][1]['port'] = $_SERVER['CFG_MYSQL_DB_PORT'];
@@ -16,6 +16,3 @@ $cfg['Servers'][1]['port'] = $_SERVER['CFG_MYSQL_DB_PORT'];
 
 $cfg['blowfish_secret']       = $_SERVER['CFG_TOOLS_PMA_BLOWFISH_KEY'];
 $cfg['Servers'][1]['hide_db'] = '(?:(?:performance|information)_schema|phpmyadmin|mysql|innodb)';
-
-$cfg['PmaAbsoluteUri'] = 'https://'.$_SERVER['HTTP_HOST'].'/---tools/pma/';
-$cfg['ForceSSL']       = true;
