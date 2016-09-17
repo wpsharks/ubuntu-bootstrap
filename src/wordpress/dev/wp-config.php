8608aa94ef9fa0127b5d5bd4ef2839bead83a1fc
jaswsinc <jaswsinc@wsharks.com>
2016-09-16 17:57:01 -0800

    Remove namespace from `wp-config.php`

--- src/wordpress/dev/wp-config.php
+++ src/wordpress/dev/wp-config.php
@@ -1,6 +1,4 @@
 <?php
-namespace WebSharks\Ubuntu\Bootstrap;
-
 // @codingStandardsIgnoreFile
 // No strict types. This must be compatible w/ PHP v5.4+.
 // i.e., This is used by the WordPress dev containers.
